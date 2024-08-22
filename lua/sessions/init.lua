104e819dcbc0965a6725cc7756f4b04b679a0107
Christoph Zirkelbach <christoph@tigion.de>
2024-08-22 21:08:28 +0200

    feat: add `Session` command for save, load and delete

--- lua/sessions/init.lua
+++ lua/sessions/init.lua
@@ -3,31 +3,48 @@ local session = require('sessions.session')
 
 local M = {}
 
--- M.exists = function() end
--- M.save = function() end
--- M.load = function() end
--- M.delete = function() end
-
 -- M.setup = config.setup
 -- M.setup = require('sessions.config').setup
 function M.setup(opts)
-  if vim.fn.has('nvim-0.10') == 0 then
+  -- check neovim version
+  if vim.fn.has('nvim-0.11') == 0 then
     local msg = 'nvim-sessions requires Neovim >= 0.10'
     vim.notify(msg, vim.log.levels.ERROR, { title = 'nvim-session' })
     return
   end
 
+  -- setup config
   require('sessions.config').setup(opts)
 
-  -- M.exists = session.exists
-  -- M.save = session.save
-  -- M.load = session.load
-  -- M.delete = session.delete
+  -- create `Session` command
+  --
+  -- NOTE: - `:h nvim_create_user_command`
+  --       - https://tui.ninja/neovim/customizing/user_commands/creating/
+  --
+  vim.api.nvim_create_user_command('Session', function(input)
+    if input.args == '' then
+      if session.exists() then
+        vim.notify('A saved session exists.')
+      else
+        vim.notify('No saved session exists.')
+      end
+      vim.notify('Usage: :Session [save|load|delete]')
+    elseif input.args == 'save' then
+      session.save()
+    elseif input.args == 'load' then
+      session.load()
+    elseif input.args == 'delete' then
+      session.delete()
+    else
+      vim.notify('Usage: :Session [save|load|delete]')
+    end
+  end, {
+    nargs = '?',
+    complete = function() return { 'save', 'load', 'delete' } end,
+    desc = 'Session [save|load|delete]',
+  })
 end
 
 M.exists = session.exists
-M.save = session.save
-M.load = session.load
-M.delete = session.delete
 
 return M
